332a0c0e2650c7b6ff0cbda58212ed1ec033aafb
ahnai17 <ahnai17@student.sdu.dk>
2023-11-25 14:39:31 +0100

    service and template instances added

--- load-balancing.tf
+++ load-balancing.tf
@@ -27,3 +27,30 @@ resource "google_compute_url_map" "default" {
   provider        = google-beta
   default_service = google_compute_backend_bucket.frontend_bucket_backend.self_link
 }
+
+# template for backend bucket
+resource "google_compute_instance_template" "instance_template" {
+  name         = "my-instance-template"
+  machine_type = "f1-micro"
+
+  disk {
+    source_image = "projects/cloud-handin-project/global/images/image-balance"  # Change to your image
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
+
+# service
+resource "google_compute_backend_service" "backend_service" {
+  name        = "backend-service"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  backend {
+    group = google_compute_instance_group_manager.instance_group.instance_group
+  }
+  health_checks = [google_compute_health_check.health_check.self_link]
+}
